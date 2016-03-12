1c9fbc4d4fbc82139fdab8c4e47678247e8b3cd1
Ram Iyer <ramky.iyer@yahoo.com>
2016-03-12 03:41:10 -0600

    using postgres instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160312064923) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string   "name"
     t.boolean  "completed"
